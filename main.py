b9acbbf740381de00a6716da349599a6ff9a0530
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-01-07 17:02:15 +0500

    Сщздана архитектура работы модулей

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pygame
+
+
+running = True
+screen = pygame.display.set_mode((500, 500))
+
+modules = {}
+
+for i in modules:
+    i.init(modules)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    for i in modules:
+        i.render(pygame.event.get())
+    pygame.display.flip()
+pygame.quit()
