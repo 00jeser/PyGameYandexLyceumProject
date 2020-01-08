a8b94d4a489bedc09d7ffe6d6f9b063bdcf7cce7
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-01-08 16:44:21 +0500

    разработана архитектура уровней

--- levelManeger.py
+++ levelManeger.py
@@ -5,6 +5,11 @@ import GameObject
 class levelManeger(GameObject.GameObject):
     def __init__(self, screen):
         super().__init__()
+        self.currentLevel = 0
+        self.levels = []
     
     def render(self, events):
-        pass
+        self.levels[self.currentLevel].render()
+    
+    def setLevel(self, n):
+        self.currentLevel = n
