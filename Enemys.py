f6c78a8389324daedd81fff0ee4c81e4de63315f
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-01-16 17:08:56 +0300

    Добавлены сервисы яндекса, как сущности

--- Enemys.py
+++ Enemys.py
@@ -7,7 +7,25 @@ class EnemyDrawer(GameObject.GameObject):
     def __init__(self, screen):
         self.screen = screen
         self.enemys = {
-            'y': load_image('y.png')
+            'y': load_image('yandex.png'),
+            'yAlis': load_image('yandexAlisa.png'),
+            'yAur': load_image('yandexAura.png'),
+            'yBrow': load_image('yandexBrowser.png'),
+            'yDisk': load_image('yandexDisk.png'),
+            'yEda': load_image('yandexEda.png'),
+            'yEfir': load_image('yandexEfir.png'),
+            'yGame': load_image('yandexIgry.png'),
+            'yMap': load_image('yandexKarti.png'),
+            'yKP': load_image('yandexKinoPoisk.png'),
+            'yMarket': load_image('yandexMarket.png'),
+            'yMusic': load_image('yandexMuzika.png'),
+            'yTrans': load_image('yandexPerevod.png'),
+            'yPlus': load_image('yandexPlus.png'),
+            'yMail': load_image('yandexPochta.png'),
+            'yRadio': load_image('yandexRadio.png'),
+            'yVid': load_image('yandexVideo.png'),
+            'yZen': load_image('yandexZen.png'),
+            'yTaxi': load_image('yandxTaxi.png'),
         }
 
     def draw(self, n, x, y, sx, sy, pole):
@@ -33,7 +51,7 @@ def load_image(name, colorkey=None):
     return image
 
 
-def text_to_screen(screen, text, x, y, size=50, color=(200, 000, 000), font_type='Comic Sans MS'):
+def text_to_screen(screen, text, x, y, size=50, color=(10, 200, 000), font_type='Comic Sans MS'):
     myfont = pygame.font.SysFont(font_type, size)
     textsurface = myfont.render(text, True, color)
     screen.blit(textsurface, (x, y))
