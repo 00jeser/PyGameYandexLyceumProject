e5844399f014f89d3bffb027af48430fbf95f67f
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-01-16 15:22:28 +0300

    123

--- EndLevel.py
+++ EndLevel.py
@@ -1,16 +1,19 @@
 import pygame
 import GameObject
 
-class EndLevel():
-    def __init__(self, screen, level = 0, win = True):
+class EndLevel(GameObject.GameObject):
+    def __init__(self, screen, level = 0, win = False):
         super().__init__()
         self.screen = screen
         if win:
             f = open('data.save', 'w')
-            f.write(level + 1)
+            f.write(str(level + 1))
         self.win = win
     
     def render(self, events):
-        pass
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.modules['levelManeger'].setLevel(0)
+        self.screen.fill((255, 255, 255))
         
 
